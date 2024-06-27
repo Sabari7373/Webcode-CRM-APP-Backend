9d91c2bd5236f382df48004b5ef355eeff31581f
Sabari arun G <115739400+Sabari7373@users.noreply.github.com>
2024-06-28 00:21:36 +0530

    Final Commit

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const jwt = require("jsonwebtoken");
 const leadsRoute = require('./routes/leadsRoute')// for leads
 const cors = require('cors');
 const corsOptions ={
-    origin:'*', 
+    origin:'https://webcode-crm-app-backend.vercel.app/', 
     credentials:true,            //access-control-allow-credentials:true
     optionSuccessStatus:200
 }
